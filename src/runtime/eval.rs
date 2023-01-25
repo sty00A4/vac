01e379ae62cfed129db6622291e0e8376515b985
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2023-01-25 19:38:55 +0100

    first u- and binary operations, remapping of binary exprs

--- src/runtime/eval.rs
+++ src/runtime/eval.rs
@@ -8,19 +8,81 @@ use scanning::token::Token;
 
 pub type EvalResult = Result<Return, String>;
 
+pub fn binary_expr(left_left_expr: &Expr, left_right_expr: &Expr, left_op: &Token, right_op: &Token, right_value: &Value) -> Option<EvalResult> {
+    let left_left_ret = eval(left_left_expr);
+    let Ok(left_left_ret) = left_left_ret else {
+        return Some(Err(left_left_ret.err().unwrap()))
+    };
+    let left_right_ret = eval(left_right_expr);
+    let Ok(left_right_ret) = left_right_ret else {
+        return Some(Err(left_right_ret.err().unwrap()))
+    };
+    match (left_left_ret, left_right_ret) {
+        (Return::Expr(left_expr), Return::Value(left_value)) => match (left_op, right_op) {
+            (Token::Add, Token::Add) | (Token::Add, Token::Sub) => match left_value.binary(right_value, right_op) {
+                Ok(value) => Some(Ok(Return::Expr(Expr::BinaryOperation {
+                    left: Box::new(left_expr), right: Box::new(value.expr()), op: left_op.clone()
+                }))),
+                Err(err) => Some(Err(err))
+            }
+            (Token::Sub, Token::Add) | (Token::Sub, Token::Sub) =>
+            match right_value.unary(left_op) {
+                Ok(right_value) => match left_value.binary(&right_value, right_op) {
+                    Ok(value) => Some(Ok(Return::Expr(Expr::BinaryOperation {
+                        left: Box::new(left_expr), right: Box::new(value.expr()), op: left_op.clone()
+                    }))),
+                    Err(err) => Some(Err(err))
+                }
+                Err(err) => Some(Err(err))
+            }
+            _ => None
+        }
+        (Return::Value(left_value), Return::Expr(left_expr)) => match left_value.binary(right_value, right_op) {
+            Ok(value) => Some(Ok(Return::Expr(Expr::BinaryOperation {
+                left: Box::new(value.expr()), right: Box::new(left_expr), op: left_op.clone()
+            }))),
+            Err(err) => Some(Err(err))
+        }
+        _ => None
+    }
+}
+
 pub fn binary(left: &Expr, right: &Expr, op: &Token) -> EvalResult {
-    let left = eval(left)?;
-    let right = eval(right)?;
-    match op {
-        
-        _ => error!("illegal binary operator '{op}'")
+    let left_ret = eval(left)?;
+    let right_ret = eval(right)?;
+    match left_ret {
+        Return::Expr(left_expr) => match right_ret {
+            Return::Expr(right_expr) => Ok(Return::Expr(Expr::BinaryOperation { left: Box::new(left_expr), right: Box::new(right_expr), op: op.clone() })),
+            Return::Value(right_value) => match &left_expr {
+                Expr::BinaryOperation { left: left_left_expr, right: left_right_expr, op: left_op } =>
+                    match binary_expr(left_left_expr.as_ref(), left_right_expr.as_ref(), &left_op, op, &right_value) {
+                        Some(res) => res,
+                        None => Ok(Return::Expr(Expr::BinaryOperation { left: Box::new(left_expr), right: Box::new(right_value.expr()), op: op.clone() }))
+                    }
+                _ => Ok(Return::Expr(Expr::BinaryOperation { left: Box::new(left_expr), right: Box::new(right_value.expr()), op: op.clone() }))
+            }
+            Return::None => error!("expected a value, got nothing")
+        }
+        Return::Value(left_value) => match right_ret {
+            Return::Expr(right_expr) => match right_expr {
+                
+                _ => Ok(Return::Expr(Expr::BinaryOperation { left: Box::new(left_value.expr()), right: Box::new(right_expr), op: op.clone() }))
+            }
+            Return::Value(right_value) => Ok(Return::Value(left_value.binary(&right_value, op)?)),
+            Return::None => error!("expected a value, got nothing")
+        }
+        Return::None => error!("expected a value, got nothing")
     }
 }
 pub fn unary(expr: &Expr, op: &Token) -> EvalResult {
     let ret = eval(expr)?;
-    match op {
-        
-        _ => error!("illegal unary operator '{op}'")
+    match ret {
+        Return::Expr(expr) => match expr {
+            
+            _ => Ok(Return::Expr(Expr::UnaryOperation { expr: Box::new(expr.clone()), op: op.clone() }))
+        }
+        Return::Value(value) => Ok(Return::Value(value.unary(op)?)),
+        Return::None => error!("expected a value, got nothing")
     }
 }
 pub fn absolute(expr: &Expr) -> EvalResult {
--- src/runtime/value.rs
+++ src/runtime/value.rs
@@ -1,4 +1,6 @@
 use std::{collections::HashSet, hash::Hash, fmt::Display};
+use crate::{*, scanning::expr::Expr};
+use scanning::token::Token;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Value {
@@ -12,6 +14,66 @@ impl Value {
             Self::Set(_, typ) => Type::Set(Box::new(typ.clone())),
         }
     }
+    pub fn expr(&self) -> Expr {
+        match self {
+            Self::Number(number) => if number.floor() == *number {
+                Expr::Int(*number as i64)
+            } else {
+                Expr::Float(*number)
+            }
+            Self::Vector(vector, _) => {
+                let mut values = vec![];
+                for value in vector.iter() {
+                    values.push(value.expr());
+                }
+                Expr::Vector(values)
+            }
+            Self::Set(set, _) => {
+                let mut values = vec![];
+                for value in set.iter() {
+                    values.push(value.expr());
+                }
+                Expr::Set(values)
+            }
+        }
+    }
+    pub fn binary(&self, other: &Self, op: &Token) -> Result<Self, Error> {
+        match (self, other) {
+            (Self::Number(number1), Self::Number(number2)) => match op {
+                Token::Add => Ok(Value::Number(number1 + number2)),
+                Token::Sub => Ok(Value::Number(number1 - number2)),
+                Token::Mult => Ok(Value::Number(number1 * number2)),
+                Token::Div => Ok(Value::Number(number1 / number2)),
+                Token::Power => Ok(Value::Number(number1.powf(*number2))),
+                _ => error!("illegal binary operator '{op}'")
+            }
+            (Self::Vector(vector, typ), other) | (other, Self::Vector(vector, typ)) => {
+                let mut values = vec![];
+                for value in vector.iter() { values.push(value.binary(other, op)?); }
+                Ok(Value::Vector(values, typ.clone()))
+            }
+            _ => error!("cannot '{op}' the values of type {} and {}", self.typ(), other.typ())
+        }
+    }
+    pub fn unary(&self, op: &Token) -> Result<Self, Error> {
+        match self {
+            Self::Number(number) => match op {
+                Token::Sub => Ok(Self::Number(-number)),
+                Token::Percent => Ok(Self::Number(number / 100.)),
+                _ => error!("illegal unary operator for number '{op}'")
+            }
+            Self::Vector(vector, typ) => {
+                let mut values = vec![];
+                for value in vector.iter() { values.push(value.unary(op)?); }
+                Ok(Value::Vector(values, typ.clone()))
+            }
+            Self::Set(set, typ) => {
+                let mut values = HashSet::new();
+                for value in set.iter() { values.insert(value.unary(op)?); }
+                Ok(Value::Set(values, typ.clone()))
+            }
+        }
+    }
 }
 impl Eq for Value {
     fn assert_receiver_is_total_eq(&self) {}
@@ -23,7 +85,7 @@ impl Hash for Value {
 impl Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Number(v) => write!(f, "{v}"),
+            Self::Number(number) => write!(f, "{number}"),
             Self::Vector(vector, _) => write!(f, "( {} )", vector.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" ")),
             Self::Set(set, _) => write!(f, "{{ {} }}", set.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" ")),
         }
